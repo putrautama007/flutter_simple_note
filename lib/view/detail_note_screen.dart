7c8fd66f812d573a9390824896672dcb73fd215d
Putra Utama <nyoman.utama@amarbank.co.id>
2021-03-15 14:39:24 +0800

    feat(simple_note): implement note bloc on detail note screen and note list

--- lib/view/detail_note_screen.dart
+++ lib/view/detail_note_screen.dart
@@ -1,47 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:simple_note/bloc/note_bloc.dart';
+import 'package:simple_note/hive/hive_service.dart';
+import 'package:simple_note/model/note_model.dart';
+import 'package:simple_note/view/note_list_screen.dart';
 
-class DetailNoteScreen extends StatelessWidget {
+class DetailNoteScreen extends StatefulWidget {
+  final bool isEdit;
+  final int noteIndex;
+  final NoteModel noteModel;
+
+  DetailNoteScreen({
+    @required this.isEdit,
+    this.noteIndex,
+    this.noteModel,
+  });
+
+  @override
+  _DetailNoteScreenState createState() => _DetailNoteScreenState();
+}
+
+class _DetailNoteScreenState extends State<DetailNoteScreen> {
   final TextEditingController _noteTitleController = TextEditingController();
   final TextEditingController _noteBodyController = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    _setupTextController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _noteTitleController.dispose();
+    _noteBodyController.dispose();
+  }
+
+  _setupTextController() {
+    if (widget.noteModel != null) {
+      _noteTitleController.text = widget.noteModel.noteTitle;
+      _noteBodyController.text = widget.noteModel.noteBody;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(
-          16.0,
+    return BlocProvider(
+      create: (_) => NoteBloc(
+        hiveService: HiveServiceImpl(
+          hive: Hive,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: _noteTitleController,
-              decoration: InputDecoration(
-                hintStyle: TextStyle(
-                  color: Colors.grey[800],
-                ),
-                hintText: "Note Title",
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            widget.isEdit ? "Edit Note" : "Create Note",
+          ),
+          leading: InkWell(
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => NoteListScreen(),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 8.0,
-              ),
-              child: TextFormField(
-                controller: _noteBodyController,
-                decoration: InputDecoration(
-                  hintStyle: TextStyle(
-                    color: Colors.grey[800],
+            child: Icon(Icons.arrow_back),
+          ),
+        ),
+        body: BlocConsumer<NoteBloc, NoteState>(
+          listener: (context, state) {
+            if (state is SaveSuccessState) {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.green,
+                  content: Text(
+                    "Save note success",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
-                  hintText: "Notes",
                 ),
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                minLines: 6,
+              );
+            } else if (state is SaveFailedState) {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.red,
+                  content: Text(
+                    "Save note failed",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              );
+            } else if (state is DeleteSuccessState) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => NoteListScreen(),
+                ),
+              );
+            } else if (state is DeleteFailedState) {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.red,
+                  content: Text(
+                    "Delete note failed",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Padding(
+              padding: EdgeInsets.all(
+                16.0,
               ),
-            ),
-          ],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    controller: _noteTitleController,
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                        color: Colors.grey[800],
+                      ),
+                      hintText: "Note Title",
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 8.0,
+                    ),
+                    child: TextFormField(
+                      controller: _noteBodyController,
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(
+                          color: Colors.grey[800],
+                        ),
+                        hintText: "Notes",
+                      ),
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      minLines: 6,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 8.0,
+                    ),
+                    child: FlatButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        if (widget.isEdit) {
+                          BlocProvider.of<NoteBloc>(context).add(
+                            UpdateNote(
+                              noteIndex: widget.noteIndex,
+                              noteModel: NoteModel(
+                                noteTitle: _noteTitleController.text,
+                                noteBody: _noteBodyController.text,
+                              ),
+                            ),
+                          );
+                        } else {
+                          BlocProvider.of<NoteBloc>(context).add(
+                            CreateNote(
+                              noteModel: NoteModel(
+                                noteTitle: _noteTitleController.text,
+                                noteBody: _noteBodyController.text,
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text(
+                        "Save",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  widget.isEdit != false
+                      ? Padding(
+                          padding: EdgeInsets.only(
+                            top: 8.0,
+                          ),
+                          child: FlatButton(
+                            color: Colors.red,
+                            onPressed: () {
+                              BlocProvider.of<NoteBloc>(context).add(
+                                DeleteNote(
+                                  noteIndex: widget.noteIndex,
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Delete",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/view/note_list_screen.dart
+++ lib/view/note_list_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:simple_note/hive/hive_service.dart';
+import 'package:simple_note/model/note_model.dart';
 import 'package:simple_note/view/detail_note_screen.dart';
+import 'package:simple_note/bloc/note_bloc.dart';
 
 class NoteListScreen extends StatefulWidget {
   @override
@@ -9,23 +14,166 @@ class NoteListScreen extends StatefulWidget {
 class _NoteListScreenState extends State<NoteListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Simple Note',
+    return BlocProvider(
+      create: (_) => NoteBloc(
+        hiveService: HiveServiceImpl(
+          hive: Hive,
         ),
-      ),
-      body: ListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) => DetailNoteScreen(),
+      )..add(
+          GetNoteList(),
+        ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Simple Note',
           ),
         ),
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: BlocConsumer<NoteBloc, NoteState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            if (state is LoadSuccessState) {
+              return _noteListWidget(
+                state.noteList,
+              );
+            } else if (state is LoadEmptyDataState) {
+              return _noDataWidget();
+            } else if (state is LoadFailedState) {
+              return _errorWidget();
+            } else {
+              return _loadingWidget();
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => DetailNoteScreen(
+                isEdit: false,
+              ),
+            ),
+          ),
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
+
+  Widget _noteListWidget(List<NoteModel> noteList) => SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: ListView.builder(
+          itemCount: noteList.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => DetailNoteScreen(
+                    isEdit: true,
+                    noteIndex: index,
+                    noteModel: noteList[index],
+                  ),
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                    15.0,
+                  ),
+                ),
+                padding: EdgeInsets.all(
+                  8.0,
+                ),
+                margin: EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  top: 16.0,
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        right: 16.0,
+                      ),
+                      child: Icon(
+                        Icons.note,
+                        color: Colors.blue,
+                        size: 48.0,
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            noteList[index].noteTitle,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            noteList[index].noteBody,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      );
+
+  Widget _noDataWidget() => SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.note,
+              color: Colors.blue,
+              size: 200.0,
+            ),
+            Text(
+              "Empty Data",
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _errorWidget() => SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error,
+              color: Colors.red,
+              size: 200.0,
+            ),
+            Text(
+              "An Error Occurred",
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _loadingWidget() => Center(
+        child: CircularProgressIndicator(),
+      );
 }
